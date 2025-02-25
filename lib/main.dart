dd80b32c280455a835c12320763ba3474cfc4a54
salman-ctrl <zirmanvictory@gmail.com>
2025-02-25 23:44:14 +0700

    edit dikit

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('this many times:'),
 
             Container(
               width: 200,
